dd1f30725960fccf71d92213271f140410f6a917
agentroberto <sherwanirobert@gmail.com>
2023-11-04 14:31:24 -0400

    Loop until the guess is correct

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,32 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    let mut input = String::new();
+    let mut is_correct: bool = false;
+    while !is_correct {
+        println!("Guess the number!");
 
-    println!("Please input your guess.");
-    io::stdin().read_line(&mut input).expect("Failed to read input");
-    // Parse the string to an appropriate number type
-    let guess: Result<u8, _> = input.trim().parse();
-    match guess {
-        Ok(number) => {
-            let mut rng = rand::thread_rng();
-            let random_number: u8 = rng.gen();
+        let mut input = String::new();
+        println!("Please input your guess.");
+        
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let guess: Result<u8, _> = input.trim().parse();
 
-            if random_number == number {
-                println!("Your guess was correct! The number is: {}", number);
+        // Parse the string to an appropriate number type
+        match guess {
+            Ok(number) => {
+                let mut rng = rand::thread_rng();
+                let random_number: u8 = rng.gen();
+                if random_number == number {
+                    is_correct = true;
+                    println!("Your guess was correct! The number is: {}", number);
+                }
+                // Do something with the parsed number
+                println!("Guessed number: {}", number);
+                println!("Try Again ");
             }
-            // Do something with the parsed number
-            println!("Guessed number: {}", number);
-            println!("Correct number: {}", random_number);
-
-        }
-        Err(e) => {
-            println!("Failed to parse input as a number: {}", e);
+            Err(e) => {
+                println!("Failed to parse input as a number: {}", e);
+            }
         }
     }
     
