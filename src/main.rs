4c95b4899bdd06b6140b18a0d6fc931e5752d5fe
agentroberto <sherwanirobert@gmail.com>
2023-11-04 14:47:09 -0400

    Add a range & exit after the correct guess

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
 use rand::Rng;
+use std::process::exit;
 
 fn main() {
     let mut is_correct: bool = false;
-    while !is_correct {
-        println!("Guess the number!");
+    println!("Guess a number between 1 and 10!");
 
+    while !is_correct {
         let mut input = String::new();
         println!("Please input your guess.");
-        
+
         io::stdin().read_line(&mut input).expect("Failed to read input");
         let guess: Result<u8, _> = input.trim().parse();
 
@@ -16,10 +17,11 @@ fn main() {
         match guess {
             Ok(number) => {
                 let mut rng = rand::thread_rng();
-                let random_number: u8 = rng.gen();
+                let random_number: u8 = rng.gen_range(0..10);
                 if random_number == number {
                     is_correct = true;
                     println!("Your guess was correct! The number is: {}", number);
+                    exit(1);
                 }
                 // Do something with the parsed number
                 println!("Guessed number: {}", number);
